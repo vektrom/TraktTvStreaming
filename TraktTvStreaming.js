9070a1b6735d093bc8b11e84c1aea4243449400b
vektrom <kenzo_aku@hotmail.com>
2015-01-18 15:02:01 +0100

    Update TraktTvStreaming.js

--- TraktTvStreaming.js
+++ TraktTvStreaming.js
@@ -1,6 +1,5 @@
-///////////////
-// From: http://stackoverflow.com/questions/2246901/how-can-i-use-jquery-in-greasemonkey-scripts-in-google-chrome
-///////////////
+
+//// From: http://stackoverflow.com/a/3550261
 // a function that loads jQuery and calls a callback function when jQuery has finished loading
 function addJQuery(callback) {
   var script = document.createElement("script");
@@ -16,9 +15,33 @@ function addJQuery(callback) {
 // the guts of this userscript
 function main() {
   // Note, jQ replaces $ to avoid conflicts.
-  var codigo = "<div style='position: absolute; z-index: 2; margin-top: 237px;'><a style='background-color: yellow; display: inline-block;' href='http://google.es'><div>Google</div></a><a style='background-color: yellow; display: inline-block;' href='http://google.es'><div>Google</div></a><a style='background-color: yellow; display: inline-block;' href='http://google.es'><div>Google</div></a><a style='background-color: yellow; display: inline-block;' href='http://google.es'><div>Google</div></a></div>";
-  $(".poster").parent().parent()
-    .prepend(codigo);
+	var codigo = "<div style='position: absolute; z-index: 2; margin-top: 237px;'><a style='background-color: yellow; display: inline-block;' href='http://google.es'><div>Google</div></a><a style='background-color: yellow; display: inline-block;' href='http://google.es'><div>Google</div></a><a style='background-color: yellow; display: inline-block;' href='http://google.es'><div>Google</div></a><a style='background-color: yellow; display: inline-block;' href='http://google.es'><div>Google</div></a></div>";
+	
+	//// From: http://stackoverflow.com/a/3146103
+	var newDiv = $("<div></div>")
+				.css("position", "absolute")
+                .css("height", "256px");
+    var newLink = $("<a></a>")
+    				.css("background-color", "yellow")
+    				.css("display", "block")
+    				.css("position", "relative")
+	    			.css("z-index", "2")
+    				.css("bottom", "0")
+    				.attr("href", "http://www.google.com/")
+    				.attr("target", "_blank")
+    					.append("Google");
+    var newLink2 = $("<a></a>")
+    				.css("background-color", "yellow")
+    				.css("display", "block")
+    				.css("position", "relative")
+	    			.css("z-index", "2")
+    				.css("bottom", "0")
+    				.attr("href", "http://www.google.com/")
+    				.attr("target", "_blank")
+    					.append("Google");
+	var allNewContent = newDiv.append(newLink).append(newLink2).append(newLink).append(newLink);
+	$(".poster").parent().parent()
+		.prepend(allNewContent);
 
 }
 
